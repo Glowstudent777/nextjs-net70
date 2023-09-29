50c7959d006fcadde1634539e66eca7f4d91102b
Glowstudent777 <cameron@themenkens.com>
2023-09-28 19:26:45 -0500

    Fixed template

--- ClientApp/pages/_app.tsx
+++ ClientApp/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+//export default function App({ Component, pageProps }: AppProps) {
+//  return <Component {...pageProps} />
+//}
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <>
+        <Component {...pageProps} />
+    </>
 }
+
+export default MyApp
